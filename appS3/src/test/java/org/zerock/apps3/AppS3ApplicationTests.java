81e892709b139291fb927e531950bd8531f5079b
kwanwoo98 <kwoo6475@gmail.com>
2024-06-05 16:00:10 +0900

    2024/06/05/15:50

---
+++ appS3/src/test/java/org/zerock/apps3/AppS3ApplicationTests.java
@@ -0,0 +1,15 @@
+package org.zerock.apps3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppS3ApplicationTests {
+
+
+
+    @Test
+    void contextLoads() {
+    }
+
+}
